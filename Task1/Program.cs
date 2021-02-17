b21d43f866329005617230d766c958fe8c253c80
yaNimotsi <yanimotsi@yanmdex.ru>
2021-02-17 21:54:48 +0300

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckEasy();
+        }
+
+        static void CheckEasy()
+        {
+            //n - задается входным значением
+            int n =50, d = 0, i = 2;
+            while (i < n)
+            {
+                if (n % i == 0)
+                {
+                    d++;
+                    i++;
+                }
+                else i++;
+            }
+
+            if (d == 0)
+                Console.WriteLine("Простое");
+            else
+                Console.WriteLine("Не простое");
+
+        }
+    }
+}
